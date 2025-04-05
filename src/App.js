70b1d244c8018024487bae1c90bc328e8f373995
CHIHCHIHTING <ct900330@gmail.com>
2025-04-05 15:19:09 +0800

    style: polish TrackerPage layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
 import RoomsPage from './pages/RoomsPage';
+import TrackerPage from './pages/TrackerPage';
 
 function App() {
     return (
@@ -11,14 +12,19 @@ function App() {
                 <header className="App-header">
                     <nav>
                         <Link to="/">主頁</Link> | <Link to="/rooms">自習室</Link>
+                        {/*新增這一行 */}
+                        | <Link to="/tracker">成效追蹤</Link>
                     </nav>
                     <Routes>
                         <Route path="/" element={<HomePage />} />
                         <Route path="/rooms" element={<RoomsPage />} />
+                        {/*新增這一行 */}
+                        <Route path="/tracker" element={<TrackerPage />} />
                     </Routes>
                 </header>
             </div>
         </Router>
     );
 }
+
 export default App;
